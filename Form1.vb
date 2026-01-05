ca75c68870b1e31c87542c2b6a10927b61dc9565
Lw <larawuu2@gmail.com>
2026-01-05 22:55:21 +0330

    tamrin1

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DialogResult = ColorDialog1.ShowDialog()
+        If DialogResult = DialogResult.OK Then
+
+        End If
+        Button1.BackColor = ColorDialog1.Color
+    End Sub
+End Class
